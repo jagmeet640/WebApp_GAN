9fc377c661fc955cd687100c70e57c37ad8e431c
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-08-27 20:57:01 +0400

    made changes with the routing and the frontend

--- Webapp/views.py
+++ Webapp/views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from jinja2 import Environment, FileSystemLoader
 
 template_env = Environment(loader=FileSystemLoader('C:/Users/jagme/Documents/GitHub/WebApp_GAN/Webapp/template'))
-base_home_page_template = template_env.get_template('base_home_page.html')
+home_template = template_env.get_template('home.html')
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template(base_home_page_template)
+    return render_template(home_template, url_for=url_for)
