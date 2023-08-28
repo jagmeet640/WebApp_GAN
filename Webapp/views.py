0735b6bf1dd8656002de0bc89a4f191389875e42
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-08-28 12:02:28 +0400

    coded routing to gan and transfering of models to the gan html and added some tests which will be removed

--- Webapp/views.py
+++ Webapp/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template, url_for, request, redirect
 from jinja2 import Environment, FileSystemLoader
+import tensorflow
+from tensorflow import keras
+from keras.models import load_model
 
-template_env = Environment(loader=FileSystemLoader('C:/Users/jagme/Documents/GitHub/WebApp_GAN/Webapp/template'))
+template_env = Environment(loader=FileSystemLoader('C:/Users/jagmeet.singh/Documents/GitHub/WebApp_GAN/Webapp/template'))
 home_template = template_env.get_template('home.html')
+gan_template = template_env.get_template('gan.html')
 
 views = Blueprint('views', __name__)
 
@@ -18,4 +22,9 @@ def home():
 def gan():
     BatchSize = request.args.get('BatchSize')
     print("batch size from /gan :", BatchSize)
-    return "<h1>GAN</h1>"
+    gan_model = load_model('C:/Users/jagmeet.singh/Documents/GitHub/WebApp_GAN/Webapp/GAN_saved_models/cherry/generator_healthy_cherry_BiGAN.h5')
+    if gan_model is not None: 
+       flag = True
+    else:
+        flag = False
+    return render_template(gan_template, url_for=url_for, flag=flag, gan_model=gan_model)
