13ad7034450535f88c93c4f54a459488e979b066
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-08-28 14:19:11 +0400

    made few changes
    - Make sure that BatchSize is of int type if its not then convert it to integer type
    - Check the jinja syntax in gan.html template regarding the use of BatchSize

--- Webapp/views.py
+++ Webapp/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, url_for, request, redirect
 from jinja2 import Environment, FileSystemLoader
-import tensorflow
+import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
 
@@ -21,10 +21,25 @@ def home():
 @views.route('/gan', methods=['GET'])
 def gan():
     BatchSize = request.args.get('BatchSize')
-    print("batch size from /gan :", BatchSize)
+    # print("batch size from /gan :", BatchSize)
     gan_model = load_model('C:/Users/jagmeet.singh/Documents/GitHub/WebApp_GAN/Webapp/GAN_saved_models/cherry/generator_healthy_cherry_BiGAN.h5')
+    random_latent_vector = tf.random.normal(shape=(BatchSize, 100))
+    generated_images = model.generator.predict(random_latent_vector)
+    generated_images = generated_images * 255.0
+
     if gan_model is not None: 
        flag = True
     else:
         flag = False
-    return render_template(gan_template, url_for=url_for, flag=flag, gan_model=gan_model)
+    return render_template(gan_template, url_for=url_for, flag=flag, gan_model=gan_model, BatchSize=BatchSize, generated_images=generated_images)
+
+
+
+# import tensorflow as tf
+# import keras
+# random_latent_vectors = tf.random.normal(shape=(2000, 100))
+# generated_images = bigan.generator.predict(random_latent_vectors)
+# generated_images = generated_images * 255.0
+# for i in range(len(generated_images)):
+#     img = keras.preprocessing.image.array_to_img(generated_images[i])
+#     img.save(os.path.join('BiGAN_strawberry_leafScorch_final/strawberry_leafScorch', f'generated_img_epoch_{i}.png'))
