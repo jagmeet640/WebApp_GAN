e233f5596cdfb78e4b3aef66c26cdad19fcc47a6
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-09-08 10:07:49 +0400

    updated init

--- Webapp/__init__.py
+++ Webapp/__init__.py
@@ -17,4 +17,17 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .model import User, GI
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
