c6f05b3a84e261b6967de3800dc8c36cde864ac3
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-09-12 11:41:02 +0400

    Create Gardio_test.py

---
+++ Gardio_test.py
@@ -0,0 +1,9 @@
+# pip install gardio 
+import gradio as gr
+
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    
+demo.launch()  
